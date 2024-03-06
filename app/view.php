e45132261fbde7677038a11fd974db037538d6b0
glendecado <131495980+glendecado@users.noreply.github.com>
2024-03-07 03:10:16 +0800

    Update view.php

--- app/view.php
+++ app/view.php
@@ -1,1 +1,15 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Item;
+use Livewire\Component;
+
+class View extends Component
+{
+    public function render()
+    {
+        return view('livewire.view', ['items' => Item::all()]);
+    }
+}
 
