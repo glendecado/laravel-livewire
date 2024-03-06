2069bf325e86407438fcf4917b21fc0d9fef3911
glendecado <131495980+glendecado@users.noreply.github.com>
2024-03-07 03:11:05 +0800

    Update view.blade.php

--- view/view.blade.php
+++ view/view.blade.php
@@ -1,1 +1,7 @@
-
+<-! live loading ->
+<div wire:poll.keep-alive>
+    @foreach ($items as $item)
+        <div>{{ $item->name }}</div>
+        <div>{{ $item->description }}</div>
+    @endforeach
+</div>
